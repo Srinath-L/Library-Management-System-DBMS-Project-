9cd52b1cb01b5f70f1f8826ebb0d92ecb9e2b68c
Srinath-L <96051591+Srinath-L@users.noreply.github.com>
2026-09-25 22:57:23 +0530

    Delete setup.js

--- setup.js
+++
@@ -1,7 +0,0 @@
-use('library_management');
-
-['authors','books','members','loans','reservations'].forEach(c => {
-  if (!db.getCollectionNames().includes(c)) db.createCollection(c);
-});
-
-printjson(db.getCollectionNames());
